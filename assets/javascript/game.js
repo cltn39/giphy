062d30dd0d8697f2bdaf16328dd7ae76fb4c57b5
gravitation <chisong@ucdavis.edu>
2019-04-18 01:16:14 -0700

    working additional img request/ working link to each gif

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,47 +2,57 @@
 const keyword = ["cat", "dog", "turtle", "lion"];
 
 function displayGif() {
-
     let gif = $(this).attr("data-name");
+    const limit = "&limit=10";
+    let offset = "&offset=" + countClick + "0"
     let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        gif + "&api_key=dc6zaTOxFJmzC&limit=10";
+        gif + "&api_key=dc6zaTOxFJmzC" + limit + offset;
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         const results = response.data;
+        console.log(response);
         console.log(results);
         for (var i = 0; i < results.length; i++) {
             // Only taking action if the photo has an appropriate rating
-            // if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-            // Creating a div for the gif
-            const gifDiv = $(`<div class=${"imgGif"}>`);
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                // Creating a div for the gif
+                const gifDiv = $(`<div class=${"imgGif"}>`);
 
-            // Storing the result item's rating
-            const rating = results[i].rating;
+                // Storing the result item's rating
+                const rating = results[i].rating;
+                // Storing the downloadable image link
+                const imgLink = results[i].images.fixed_height.url;
+                // Creating a paragraph tag with the result item's rating
+                const paragraph = $("<p>").text(`Rating: ${rating}`);
 
-            // Creating a paragraph tag with the result item's rating
-            const paragraph = $("<p>").text(`Rating: ${rating}`);
+                // Creating an image tag
+                const gifImage = $("<img>");
+                // Creating a download tag
+                const gifDownload = $("<a>");
+                // Creating space between img
+                const spacing = $("<br>");
 
-            // Creating an image tag
-            const gifImage = $("<img>");
-            
-            // Creating space between img
-            const spacing = $("<br>");
-            
-            // Giving the image tag an src attribute of a proprty pulled off the
-            // result item
-            gifImage.attr("src", results[i].images.fixed_height.url);
+                // Giving the image tag an src attribute of a proprty pulled off the
+                // result item
+                gifImage.attr("src", imgLink);
+                
+                gifDownload.attr({
+                    href: imgLink,
+                    download: true
+                });
+                // Appending the paragraph and gifImage we created to the "gifDiv" div we created
+                gifDiv.append(paragraph);
+                gifDiv.append(spacing);
+                gifDiv.append(gifDownload);
+                // Appending gifImage into gifDownload so that when you click gif, it will download.
+                gifDownload.append(gifImage);
 
-            // Appending the paragraph and gifImage we created to the "gifDiv" div we created
-            gifDiv.append(paragraph);
-            gifDiv.append(spacing);
-            gifDiv.append(gifImage);
-
-            // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-            $("#gif").prepend(gifDiv);
-            //   }
+                // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                $("#gif").prepend(gifDiv);
+            }
         }
     });
 
@@ -76,7 +86,6 @@ $("#add-gif").on("click", function (event) {
     event.preventDefault();
     // This line grabs the input from the textbox
     var gifphy = $("#gif-input").val().trim();
-
     // Adding movie from the textbox to our array
     keyword.push(gifphy);
 
@@ -90,11 +99,14 @@ $(document).on("click", ".movie-btn", displayGif);
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
 
-//plan to add additonal features
 //Track how many times I have clicked 
-//Write a function that multiplies  
-//Built in api query string 'offset' 
-//Html tag that has download 
-//Built into the anchor tag 
+let countClick = 0;
+//Write a function that request more gif
+$(".movie-btn").on("click", function () {
+    countClick++;
+    console.log(countClick);
+})
+//Html tag that has download
+//Built-in the anchor tag 
 //Google html anchor download 
 //possibly store data into local storage or session storage instead.
